9ea18a308c2baa4d9fec2776cdaf702efa67a7a6
Cao Nga Bảo Uyên <nguyenanhbao714@gmail.com>
2025-03-18 11:17:03 +0700

    Update code before deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/home-custom-printing-pro/', // 👈 Thêm dòng này
   plugins: [react()],
 })
