cad83907eff9a514d6e994ad81b21066601a9ba4
Cao Nga Bảo Uyên <nguyenanhbao714@gmail.com>
2025-03-19 15:38:04 +0700

    setup

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/home-custom-printing-pro/', // 👈 Thêm dòng này
   plugins: [react()],
-  build: {
-    outDir: './build',
-    emptyOutDir: true, // also necessary
-  }
-})
+});
