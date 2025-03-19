f735a47652a0b92cf7eb3adaaa07970ea35ac580
Cao Nga Bảo Uyên <nguyenanhbao714@gmail.com>
2025-03-19 16:27:26 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/home-custom-printing-pro/', // 👈 Thêm dòng này
-  // plugins: [react()],
-  base: '/home-custom-printing-pro/',
+  base: '/home-custom-printing-pro/', // 👈 Thêm dòng này
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        // 👇 tạo thêm 404.html khi build
-        404: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
 });
