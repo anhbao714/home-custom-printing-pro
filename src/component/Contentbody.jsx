001c6b20265c4aaf91edfb2219b7d4c510704fc2
Cao Nga Bảo Uyên <nguyenanhbao714@gmail.com>
2025-03-21 18:09:47 +0700

    update

--- src/component/Contentbody.jsx
+++ src/component/Contentbody.jsx
@@ -249,7 +249,7 @@ export default function Body() {
         </div>
 
         <div className="row product w-full h-[2270px] md:h-[1135px] lg:h-[500px] flex p-5">
-          <div className="grid grid-cols-1 w-full h-full lg:grid-cols-4 md:grid-cols-3 gap-4 px-4">
+          <div className="grid grid-cols-1 w-full h-[2270px] lg:grid-cols-4 md:grid-cols-3 gap-4 px-4">
             {products.slice(0, 4).map((product) => (
               <div
                 key={product.id}
